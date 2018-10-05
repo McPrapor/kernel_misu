0b32d7f463d5e1a4dfbf215ec706fe46cb1713ec
Max Terleckiy <max.terleckiy@gmail.com>
2018-10-05 21:54:52 +0200

    ccci debug

--- drivers/misc/mediatek/ccci_util/ccci_util_log.h
+++ drivers/misc/mediatek/ccci_util/ccci_util_log.h
@@ -3,11 +3,11 @@
 
 /* No MD id message part */
 #define CCCI_UTIL_DBG_MSG(fmt, args...)\
-pr_debug("[ccci0/util]" fmt, ##args) 
+	pr_err("[ccci0/util]" fmt, ##args) 
 //do {} while (0)/*	pr_debug("[ccci0/util]" fmt, ##args) */
 
 #define CCCI_UTIL_INF_MSG(fmt, args...)\
-pr_debug("[ccci0/util]" fmt, ##args) 
+	pr_err("[ccci0/util]" fmt, ##args) 
 //do {} while (0)/*	pr_debug("[ccci0/util]" fmt, ##args) */
 
 #define CCCI_UTIL_ERR_MSG(fmt, args...)\
@@ -15,15 +15,16 @@ pr_debug("[ccci0/util]" fmt, ##args)
 
 /* With MD id message part */
 #define CCCI_UTIL_DBG_MSG_WITH_ID(id, fmt, args...)\
-	pr_debug("[ccci%d/util]" fmt, (id+1), ##args)
+	pr_err("[ccci%d/util]" fmt, (id+1), ##args)
 //do {} while (0)/*	pr_debug("[ccci%d/util]" fmt, (id+1), ##args) */
 
 #define CCCI_UTIL_INF_MSG_WITH_ID(id, fmt, args...)\
-	pr_debug("[ccci%d/util]" fmt, (id+1), ##args)
+	pr_err("[ccci%d/util]" fmt, (id+1), ##args)
 //do {} while (0)/*	pr_debug("[ccci%d/util]" fmt, (id+1), ##args) */
 
 #define CCCI_UTIL_NOTICE_MSG_WITH_ID(id, fmt, args...) \
-	pr_notice("[ccci%d/util]" fmt, (id+1), ##args)
+	pr_err("[ccci%d/util]" fmt, (id+1), ##args)
+//	pr_notice("[ccci%d/util]" fmt, (id+1), ##args)
 
 #define CCCI_UTIL_ERR_MSG_WITH_ID(id, fmt, args...)\
 	pr_err("[ccci%d/util]" fmt, (id+1), ##args)
