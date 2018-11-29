741af58dc304abfef0d97ed2427f6714135f22a7
Max Terleckiy <max.terleckiy@gmail.com>
2018-11-29 11:54:31 +0100

    cm36686-new fix

--- drivers/misc/mediatek/alsps/cm36686-new/cm36686.c
+++ drivers/misc/mediatek/alsps/cm36686-new/cm36686.c
@@ -422,13 +422,16 @@ static int cm36686_get_ps_value(struct cm36686_priv *obj, u16 ps)
 
 	val = intr_flag;	/* value between high/low threshold should sync. with hw status. */
 
-	if (ps > atomic_read(&obj->ps_thd_val_high))
+	if (ps > atomic_read(&obj->ps_thd_val_high)){
 		val = 0;	/*close */
-	else if (ps < atomic_read(&obj->ps_thd_val_low))
+		APS_DBG("ALS debug get_ps_value PS:  val = 0 CLOSE");
+	}
+	else if (ps < atomic_read(&obj->ps_thd_val_low)){
+		APS_DBG("ALS debug get_ps_value PS:  val = 1 FAR");
 		val = 1;	/*far away */
-
+	}
 	APS_DBG("ALS debug get_ps_value PS:  %05d => %05d [M]\n", ps, val);
-	APS_DBG("ALS debug get_ps_value PS: thlow: %x thhigh: %x", atomic_read(&obj->ps_thd_val_low), atomic_read(&obj->ps_thd_val_high));
+	APS_DBG("ALS debug get_ps_value PS: thlow: %05d thhigh: %05d", atomic_read(&obj->ps_thd_val_low), atomic_read(&obj->ps_thd_val_high));
 	if (atomic_read(&obj->ps_suspend)) {
 		invalid = 1;
 		APS_DBG("ALS debug get_ps_value PS ps_suspend");
